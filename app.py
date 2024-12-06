b63f7c6aaa04081044d25f62d7873b8be7b0b78e
gabru12 <arcanjogabriel2005@gmail.com>
2024-12-06 11:55:31 +0000

    commit

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def list_billionaires():
     global DB
     billionaires = execute(
       '''
-      SELECT b.billionaire_id, b.fullname, b.age, b.wealth, b.city_id, c2.country_id, c.company_id 
+      SELECT b.billionaire_id, b.fullname, b.age, b.wealth, c1.city_of_residence, c2.country_of_residence, c.company_id 
       FROM Billionaires b
       JOIN Billionaire_Companies bc on b.billionaire_id = bc.billionaire_id
       JOIN Companies c on c.company_id = bc.company_id
@@ -76,9 +76,25 @@ def list_billionaires():
       ''')
     
     columns = [desc[0] for desc in DB['cursor'].description]
-    rows = [dict(zip(columns, row)) for row in DB['cursor'].fetchall()]
     
-    return render_template('doEverything-list.html', columns=columns, rows=rows)
+    references = execute(
+      '''
+      SELECT b.billionaire_id, b.fullname, b.age, b.wealth, b.city_id, c1.city_of_residence, c2.country_id, c2.country_of_residence, c.company_id 
+      FROM Billionaires b
+      JOIN Billionaire_Companies bc on b.billionaire_id = bc.billionaire_id
+      JOIN Companies c on c.company_id = bc.company_id
+      JOIN Cities c1 on c1.city_id = b.city_id
+      JOIN States s on s.state_id = c1.state_id
+      JOIN Countries c2 on c2.country_id = s.country_id
+      ORDER BY b.billionaire_id
+      ''')
+    
+    columns1 = [desc[0] for desc in DB['cursor'].description]
+    references = [dict(zip(columns1, row)) for row in DB['cursor'].fetchall()]
+
+    first_key = next(iter(references[0]))  
+    
+    return render_template('doEverything-list.html', columns=columns, references=references, first_key=first_key)
 
 
 @APP.route('/billionaires/<int:id>/')
@@ -127,9 +143,11 @@ def list_companies():
     ''')
 
     columns = [desc[0] for desc in DB['cursor'].description]
-    rows = [dict(zip(columns, row)) for row in DB['cursor'].fetchall()]
+    references = [dict(zip(columns, row)) for row in DB['cursor'].fetchall()]
 
-    return render_template('doEverything-list.html', columns=columns, rows=rows)
+    first_key = next(iter(references[0]))  
+
+    return render_template('doEverything-list.html', columns=columns, references=references, first_key=first_key)
 
 
 @APP.route('/companies/<int:id>/')
@@ -177,15 +195,18 @@ def search_companies(expr):
 def list_countries():
     global DB
     countries = execute('''
-      SELECT country_id, country_of_residence, continent_id  
-      FROM Countries
+      SELECT c.country_id, c.country_of_residence, c1.continent  
+      FROM Countries c
+      JOIN Continents c1 on c1.continent_id = c.continent_id
       ORDER BY country_id
     ''')
 
     columns = [desc[0] for desc in DB['cursor'].description]
-    rows = [dict(zip(columns, row)) for row in DB['cursor'].fetchall()]
+    references = [dict(zip(columns, row)) for row in DB['cursor'].fetchall()]
 
-    return render_template('doEverything-list.html', columns=columns, rows=rows)
+    first_key = next(iter(references[0]))  
+
+    return render_template('doEverything-list.html', columns=columns, references=references, first_key=first_key)
 
 @APP.route('/countries/<int:id>/')
 def view_country_details(id):
@@ -235,17 +256,27 @@ def search_countries(expr):
 def list_cities():
     global DB
     countries = execute('''
-      SELECT c.city_id, c.city_of_residence, c.state_id, c1.country_id  
+      SELECT c.city_id, c.city_of_residence, s.res_state, c1.country_of_residence  
       FROM Cities c
       JOIN States s on s.state_id = c.state_id
       JOIN Countries c1 on c1.country_id = s.country_id
       ORDER BY c.city_id
     ''')
-
     columns = [desc[0] for desc in DB['cursor'].description]
-    rows = [dict(zip(columns, row)) for row in DB['cursor'].fetchall()]
 
-    return render_template('doEverything-list.html', columns=columns, rows=rows)
+    references = execute('''
+      SELECT c.city_id, c.city_of_residence, c.state_id, s.res_state, c1.country_id, c1.country_of_residence  
+      FROM Cities c
+      JOIN States s on s.state_id = c.state_id
+      JOIN Countries c1 on c1.country_id = s.country_id
+      ORDER BY c.city_id
+    ''') 
+    columns1 = [desc[0] for desc in DB['cursor'].description]
+    references = [dict(zip(columns1, row)) for row in DB['cursor'].fetchall()]
+
+    first_key = next(iter(references[0])) 
+
+    return render_template('doEverything-list.html', columns=columns, references=references, first_key=first_key)
 
 @APP.route('/cities/<int:id>/')
 def view_city_details(id):
@@ -300,9 +331,11 @@ def list_states():
     ''')
 
     columns = [desc[0] for desc in DB['cursor'].description]
-    rows = [dict(zip(columns, row)) for row in DB['cursor'].fetchall()]
+    references = [dict(zip(columns, row)) for row in DB['cursor'].fetchall()]
 
-    return render_template('doEverything-list.html', columns=columns, rows=rows)
+    first_key = next(iter(references[0]))  
+
+    return render_template('doEverything-list.html', columns=columns, references=references, first_key=first_key)
 
 @APP.route('/states/<int:id>/')
 def view_state_details(id):
@@ -343,6 +376,84 @@ def search_state(expr):
   
   return render_template('state-search.html',
            search=search,states=states)
+
+@APP.route('/1_Question', methods=['POST'])
+def question1():
+    global DB
+    n_industries = request.form.get('value1')
+    try:
+        n_industries = int(n_industries)
+    except ValueError:
+        return "Invalid input! Please enter a valid number.", 400
+    query = '''
+            SELECT c.industry, SUM(b.wealth) AS total_wealth_millions
+            FROM Billionaire_Companies bc
+            JOIN Billionaires b ON bc.billionaire_id = b.billionaire_id
+            JOIN Companies c ON bc.company_id = c.company_id
+            GROUP BY c.industry
+            ORDER BY COUNT(b.billionaire_id) DESC
+            LIMIT ?;
+        '''
+    try:
+        question = execute(query, [n_industries])
+        columns = [desc[0] for desc in DB['cursor'].description]
+        rows = [dict(zip(columns, row)) for row in DB['cursor'].fetchall()]
+        return render_template('sql_results.html', query=query, columns=columns, rows=rows)
+    except sqlite3.Error as e:
+        error_message = str(e)
+        return render_template('sql_results.html', query=query, error=error_message)
+
+
+@APP.route('/2_Question', methods=['POST'])
+def question2():
+    global DB
+    n_billionaires = request.form.get('value1')
+    try:
+        n_billionaires = int(n_billionaires)
+    except ValueError:
+        return "Invalid input! Please enter a valid number.", 400
+    query = '''
+            SELECT b.fullname, b.age, c2.country_id, c.industry , b.wealth AS 'wealth (millions)'
+            FROM Billionaire_Companies bc
+            JOIN Companies c ON bc.company_id = c.company_id
+            JOIN Billionaires b ON bc.billionaire_id = b.billionaire_id
+            JOIN Cities c1 on c1.city_id = b.city_id
+            JOIN States s on s.state_id = c1.state_id
+            JOIN Countries c2 on c2.country_id = s.country_id
+            ORDER BY b.age
+            LIMIT ?;
+        '''
+    try:
+        question = execute(query, [n_billionaires])
+        columns = [desc[0] for desc in DB['cursor'].description]
+        rows = [dict(zip(columns, row)) for row in DB['cursor'].fetchall()]
+        return render_template('sql_results.html', query=query, columns=columns, rows=rows)
+    except sqlite3.Error as e:
+        error_message = str(e)
+        return render_template('sql_results.html', query=query, error=error_message)
+
+@APP.route('/3_Question', methods=['POST'])
+def question3():
+    global DB
+    query = '''
+            SELECT cont.continent, 
+            AVG(b.wealth) AS avg_wealth, 
+            AVG(b.age) AS avg_age
+            FROM Billionaires b
+            JOIN Cities c on c.city_id = b.city_id
+            JOIN States s on s.state_id = c.state_id
+            JOIN Countries co ON s.country_id = co.country_id
+            JOIN Continents cont ON co.continent_id = cont.continent_id
+            GROUP BY cont.continent;
+        '''
+    try:
+        question = execute(query)
+        columns = [desc[0] for desc in DB['cursor'].description]
+        rows = [dict(zip(columns, row)) for row in DB['cursor'].fetchall()]
+        return render_template('sql_results.html', query=query, columns=columns, rows=rows)
+    except sqlite3.Error as e:
+        error_message = str(e)
+        return render_template('sql_results.html', query=query, error=error_message)
 
 PROHIBITEDCOMMANDS = ["DROP", "DELETE", "UPDATE", "INSERT", "ALTER", "TRUNCATE", "REPLACE", "CREATE"]
 
